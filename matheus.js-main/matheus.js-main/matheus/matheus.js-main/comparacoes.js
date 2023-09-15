5794ad750e914f8bfc096f00dbb40cdcec23eab2
Matheusbritocoelho <127303037+Matheusbritocoelho@users.noreply.github.com>
2023-09-15 09:56:21 -0300

    Add files via upload

---
+++ matheus.js-main/matheus.js-main/matheus/matheus.js-main/comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparação implicita)
+
+const numerom= 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// typeof 
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == so compara o valor
+// === compra o valor e o tipo de dado
+
+// conversão explicita
+
+Number()
+String()
